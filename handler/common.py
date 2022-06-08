25823b644cdedafd3380e53d9ac42e9f0420d4c8
Семён Капустников <s.v.kapustnikov@gmail.com>
2022-06-08 13:12:42 +0300

    add loggig

--- handler/common.py
+++ handler/common.py
@@ -1,8 +1,12 @@
+import logging
+import sqlite3
+
 from add_circles import make_chord
 from aiogram import Dispatcher, types
 from create_bot import bot
-import sqlite3
-# @dp.message_handler()
+
+
+logging.basicConfig(level=logging.INFO, filename='main.log', filemode='w')
 
 
 def find_chord(request):
@@ -15,21 +19,26 @@ def find_chord(request):
     cursor.execute(sqlite_select_query)
     records = cursor.fetchall()
     cursor.close()
-    print(records)
     return records
 
 
 async def get_message(message: types.Message):
     chat_id = message.chat.id
+    logging.info(f'Сообщение {message.text} получено из {chat_id}')
     img_chords = find_chord(message.text)
-    for y in img_chords:
-        chord = y[0]
-        positions = y[1].split('P')
-        first_fing = y[2].split()
-        img_type = y[3]
-        image = open(make_chord(positions, img_type=img_type,
-                                chord=chord, first_fing=first_fing), 'rb')
-        await bot.send_photo(chat_id=chat_id, photo=image)
+    if len(img_chords):
+        for y in img_chords:
+            chord = y[0]
+            positions = y[1].split('P')
+            first_fing = y[2].split()
+            img_type = y[3]
+            image = open(make_chord(positions, img_type=img_type,
+                                    chord=chord, first_fing=first_fing), 'rb')
+            await bot.send_photo(chat_id=chat_id, photo=image)
+    else:
+        await bot.send_message(chat_id=chat_id,
+                               text='Я еще такого аккорда не знаю')
+        logging.error(f'Аккорда {message.text} нет в БД')
 
 
 def register_handlers_common(dp: Dispatcher):
